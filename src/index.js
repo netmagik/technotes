bbfe48450af6b27b0554b31fe0212ff623b52510
Irina <irina@netmagik.com>
2022-11-13 10:43:31 -0500

    remove immer

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
-import { processResult } from "immer/dist/internal";
 
 // Disable React Dev Tools only for Production when we deploy
-if (processResult.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
